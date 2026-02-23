fff9d5c9aeb190f884a467373e94f68774bd7690
Kat Ramos <katramos@Kats-MacBook-Air.local>
2026-02-22 23:24:20 -0700

    Add TestimonialsSection to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import ProjectsSection from "../components/ProjectsSection";
 import AboutSection from "../components/AboutSection";
 import ContactSection from "../components/ContactSection";
 import Services from "./Services";
+import TestimonialsSection from "../components/TestimonialsSection";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Services />
 
       <ProjectsSection />
+      <TestimonialsSection />
       <AboutSection />
       <ContactSection />
     </>
